5eb52fd7eddcffed379bd30e650568a1e2f0b120
lavanyamuvva1 <lavanyamuvva1@gmail.com>
2023-08-17 15:56:05 +0530

    committed new changes

---
+++ GymAuthenticationService-1/src/test/java/com/epam/GymAuthenticationService1ApplicationTests.java
@@ -0,0 +1,11 @@
+package com.epam;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@EnableAutoConfiguration(exclude = {KafkaAutoConfiguration.class, DataSourceAutoConfiguration.class})
+public class GymAuthenticationService1ApplicationTests {
+}
